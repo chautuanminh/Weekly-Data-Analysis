eb559437b285e86912a70f581db1bfa1e84caca9
chautuanminh <tuanminhchau31@gmail.com>
2025-09-15 22:46:35 +0700

    a whole new world

---
+++ vsl/testing.py
@@ -0,0 +1,27 @@
+def detect_landmarks(path):
+    """Detects landmarks in the file."""
+    from google.cloud import vision
+
+    client = vision.ImageAnnotatorClient()
+
+    with open(path, "rb") as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    response = client.landmark_detection(image=image)
+    landmarks = response.landmark_annotations
+    print("Landmarks:")
+
+    for landmark in landmarks:
+        print(landmark.description)
+        for location in landmark.locations:
+            lat_lng = location.lat_lng
+            print(f"Latitude {lat_lng.latitude}")
+            print(f"Longitude {lat_lng.longitude}")
+
+    if response.error.message:
+        raise Exception(
+            "{}\nFor more info on error messages, check: "
+            "https://cloud.google.com/apis/design/errors".format(response.error.message)
+        )
